c5db380f2c622f7f1873ba8cf7dd99fc7d0e6c78
Blen Sium <105584167+blenSium@users.noreply.github.com>
2022-11-27 23:44:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = 8000;
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://recipes-blog-nu.vercel.app",
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );

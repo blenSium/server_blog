2b0479d6c823fc330e5825f09a51bed0f425c41e
Blen Sium <105584167+blenSium@users.noreply.github.com>
2022-11-29 18:51:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,12 +9,21 @@ const commentsRouter = require('./routers/commentsRouter')
 const app = express();
 const port = 8000;
 app.use(express.json());
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-  })
-);
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://yoursite.com");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+// app.use(
+//   cors({
+//     origin: "*",
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//   })
+// );
 app.use(express.static('public'));
 
 const storage = multer.diskStorage({

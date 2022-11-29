cfa74c58e64c6fc6caf4883b1a2942def98fa81f
Blen Sium <105584167+blenSium@users.noreply.github.com>
2022-11-29 18:52:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 const port = 8000;
 app.use(express.json());
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://yoursite.com");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"

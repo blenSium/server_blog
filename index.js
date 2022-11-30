cc6f47f8a38f247676b88184dc47ddb7c5218ae3
shelly <shely2111@gmail.com>
2022-11-30 13:22:54 +0200

    first commit & push

--- index.js
+++ index.js
@@ -9,15 +9,15 @@ const commentsRouter = require('./routers/commentsRouter')
 const app = express();
 const port = 8000;
 app.use(express.json());
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://recipes-blog-nu.vercel.app");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "https://recipes-blog-nu.vercel.app");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
+app.use(cors())
 app.use(express.static('public'));
 
 const storage = multer.diskStorage({

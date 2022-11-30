15216c611f8b63707e7c637f9057f279d5a37caf
Blen Sium <105584167+blenSium@users.noreply.github.com>
2022-11-30 10:28:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 const port = 8000;
 app.use(express.json());
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "https://recipes-blog-nu.vercel.app");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
